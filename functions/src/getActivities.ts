44af1fc20ecc9d15da609e7f11ec0c4bef018c52
dhruv-pandey <dhruv.pandey93@gmail.com>
2024-10-19 11:35:43 +0300

    Update functions

--- functions/src/getActivities.ts
+++ functions/src/getActivities.ts
@@ -5,35 +5,30 @@ type Error = { error: string };
 
 type RequestData = {
   searchTerm?: string;
-  searchExact?: boolean;
 };
 
-type ResponseData =
-  | Error
-  | {
-      activities: string[];
-    };
+type ResponseData = Error | string[];
 
 /**
  * Returns all the activities with provided filter inputs
  * @param searchTerm the activities that needs to be searched. If nothing provided all the activities will be returned
- * @param searchExact should perform exact or fuzzy search for activityName
  * @returns List of all activities for selected search criteria
  */
 export const getActivities = onCall<RequestData, Promise<ResponseData>>(
   async (request) => {
-    const { searchTerm, searchExact = false } = request.data;
+    const { searchTerm } = request.data;
+    const activities: string[] = [];
 
-    await getFirestore()
+    const snapshot = await getFirestore()
       .collection("activities")
-      .where(FieldPath.documentId(), "in", searchTerm)
-      .get()
-      .then((result) => {
-        result.forEach((doc) => {
-          console.log(doc.id, doc.data());
-        });
-      });
+      .where(FieldPath.documentId(), ">=", searchTerm)
+      .where(FieldPath.documentId(), "<", searchTerm + "\uf8ff")
+      .get();
 
-    return [];
+    console.log(":::::::::::::::", snapshot.docs);
+
+    snapshot.docs.forEach((doc) => activities.push(doc.data()["data"]));
+
+    return activities;
   }
 );
