d5075f4a8f2f90258cef406c03185cfce7d82c54
dhruv-pandey <dhruv.pandey93@gmail.com>
2024-10-21 09:25:28 +0300

    Initialize app

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,5 @@
+import { initializeApp } from "firebase-admin/app";
+
 export { createActivity } from "./createActivity";
 export { getActivities } from "./getActivities";
 export { createUser } from "./createUser";
@@ -5,3 +7,5 @@ export { deleteUser } from "./deleteUser";
 export { getUsers } from "./getUsers";
 export { sendNotification } from "./sendNotification";
 export { updateUser } from "./updateUser";
+
+initializeApp();
